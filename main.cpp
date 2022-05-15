a8ba773b03dccc3e304735994c41067991a5e3f6
Nathan Rignall <rignallnathan@gmail.com>
2022-05-15 18:02:46 +0100

    simple tcp output

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-#include <thread>
+#include <unistd.h>	
 
 #include <openmotion_led_hat_control.h>
-
-#include <unistd.h>		
+#include <openmotion_network.h>
 
 using namespace open_motion;
 
@@ -12,12 +11,12 @@ int main(){
 
     led_hat led_hat(0x17);
 
-    printf("test read %i \n", led_hat.get_ir_led_brightness());
+    led_hat.set_ring_led_brightness(0xFF);
+    led_hat.set_ir_led_brightness(0x90);
+
+    system("libcamera-vid -t 0 -b 10000000 --width 1280 --height 720 --intra 60 --rotation 180 --inline --listen -o tcp://0.0.0.0:9000");
 
     while (true) {
-        printf("some \n");
-        
-        sleep(5);
-        printf("test read %i \n", led_hat.get_ir_led_brightness());
+        sleep(100);
     };
 }
