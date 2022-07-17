6c66129e92351d2f457bcc551d9e40eb19ca769a
Nathan Rignall <rignallnathan@gmail.com>
2022-07-17 18:10:20 +0100

    add basic camera support

--- main.cpp
+++ main.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
-#include <unistd.h>	
+#include <unistd.h>
 
 #include <openmotion_led_hat_control.h>
 #include <openmotion_network.h>
+#include <openmotion_libcamera.hpp>
+
+#include <opencv2/opencv.hpp>
 
 using namespace open_motion;
 
-int main(){
-    std::printf("hello");
+int main()
+{
 
     led_hat led_hat(0x17);
+    cv::Mat image;
+    PiCamera camera;
 
-    led_hat.set_ring_led_brightness(0xFF);
-    led_hat.set_ir_led_brightness(0x90);
+    camera.options->video_width = 1024;
+    camera.options->video_height = 768;
+    camera.options->framerate = 5;
+    camera.options->verbose = true;
 
-    system("libcamera-vid -t 0 -b 10000000 --width 1280 --height 720 --intra 60 --rotation 180 --inline --listen -o tcp://0.0.0.0:9000");
+    led_hat.set_ring_led_brightness(0x40);
+    led_hat.set_ir_led_brightness(0x00);
 
-    while (true) {
-        sleep(100);
-    };
+    cv::namedWindow("Video", cv::WINDOW_NORMAL);
+    camera.startVideo();
+    int ch = 0;
+    while (ch != 27)
+    {
+        if (!camera.getVideoFrame(image, 1000))
+        {
+            std::cout << "Timeout error" << std::endl;
+        }
+        else
+        {
+            cv::imshow("Video", image);
+            ch = cv::waitKey(10);
+        }
+    }
+    camera.stopVideo();
+    cv::destroyWindow("Video");
 }
